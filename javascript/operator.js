96f78c96dec9a64e56c605b030beb747506477f8
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-10-27 20:01:04 +0530

    Arithmetic Operators

---
+++ javascript/operator.js
@@ -0,0 +1,29 @@
+//Arithmetic Operators
+console.log(10 + 40);// Addition
+console.log(20 - 40);// Subtracton
+console.log(30 * 2);//Multiplication
+console.log(30 / 20); //Quetient
+console.log(50 % 20); //Modules
+console.log(30 ** 3 ); // exponential
+
+//Increment and Decrement 
+
+//INCREMENT
+let num = 10;
+num = num + 1;
+console .log(num);
+//OR
+let num1 = 15;
+num1++; //Preincrement
+++num1;//Postincrement
+console.log (num1);
+
+//DECREMENT
+let num2 = 10;
+num = num -1;
+console.log(num2);
+//OR
+let num3 = 40;
+num--;//PostDecrement
+--num;//PreDecrement
+console.log(num3);
