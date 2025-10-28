f31df7789d63f54ae5d97807fe83a675ae741803
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-10-28 13:44:01 +0530

    Concatination and String Interpolation

---
+++ javascript/concate.js
@@ -0,0 +1,17 @@
+let str = "Hello ";
+let str1 = "World";
+
+// let output = str+ srt1;
+// console.log(output);
+console.log(str + " " + str1 + " " + "Welcome to our website");
+//Template literal (` `)
+let new3 = `javascript`
+
+//String interpolation
+let new2  =`${new3} is a scripting language` ;
+console.log(new2);
+
+let multi = 5
+let mul = 1;
+let mull = `${multi}*${mul}=${multi*mul}`;
+console.log(mull);
