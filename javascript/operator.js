df02d2cc692afe8ebd27ca55cda6b3d7efe5863a
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-10-28 13:21:12 +0530

    Logical Operators

--- javascript/operator.js
+++ javascript/operator.js
@@ -4,23 +4,23 @@ console.log(20 - 40);// Subtracton
 console.log(30 * 2);//Multiplication
 console.log(30 / 20); //Quetient
 console.log(50 % 20); //Modules
-console.log(30 ** 3 ); // exponential
+console.log(30 ** 3); // exponential
 
 //Increment and Decrement 
 
 //INCREMENT
 let num = 10;
 num = num + 1;
-console .log(num);
+console.log(num);
 //OR
 let num1 = 15;
 num1++; //Preincrement
 ++num1;//Postincrement
-console.log (num1);
+console.log(num1);
 
 //DECREMENT
 let num2 = 10;
-num = num -1;
+num = num - 1;
 console.log(num2);
 //OR
 let num3 = 40;
@@ -38,7 +38,7 @@ newnum = newnum++ + ++newnum; // 10 + 12 = 22
 console.log(newnum);
 
 let newnum1 = 1;
-newnum1= ++newnum1 + ++newnum1; // 2+ 3 = 5
+newnum1 = ++newnum1 + ++newnum1; // 2+ 3 = 5
 console.log(newnum1);
 
 
@@ -49,12 +49,12 @@ console.log(newnum1);
 
 let number = 30;
 number = --number - --number // 29 - 28 = 1
-console.log(number); 
+console.log(number);
 
 let number1 = 9;
 number1 = number1-- - number1--
 
-console.log(number1,number);
+console.log(number1, number);
 
 //Assingment Operators 
 //Addition
@@ -79,7 +79,7 @@ console.log(mul);
 //Division
 let div = 3;
 let divisionval = 2;
-div /= divisionval ;
+div /= divisionval;
 console.log(div);
 
 //Modules
@@ -95,15 +95,45 @@ expo **= exponenval;
 console.log(expo);
 
 // Relational Operators or Comparisional Operators
-console.log( 10 < 20); //Less than <
-console.log(20>30); //Grater than >
-console.log(20>=10); //Grater than or equal to >=
-console.log(50<=30); //Less than or equal to <=
-console.log(50==50); // equal to == ( to compare only values)
-console.log(50==="50"); // equal to === ( to compare only values and data types)
+console.log(10 < 20); //Less than <
+console.log(20 > 30); //Grater than >
+console.log(20 >= 10); //Grater than or equal to >=
+console.log(50 <= 30); //Less than or equal to <=
+console.log(50 == 50); // equal to == ( to compare only values)
+console.log(50 === "50"); // equal to === ( to compare only values and data types)
 // in not equal to if there is a different value the first preference is for value or else the data is different the first priority will be for data type
-console.log(50 !=50); // not equal to != ( to compare only values )
-console.log(50 !==50); // Strict not equal to != ( to compare only values and data types)
-console.log(50 !=="50"); // Strict not equal to != ( to compare only values and data types)
+console.log(50 != 50); // not equal to != ( to compare only values )
+console.log(50 !== 50); // Strict not equal to != ( to compare only values and data types)
+console.log(50 !== "50"); // Strict not equal to != ( to compare only values and data types)
 
+// Logical Operators
+// logical And &&
 
+/* cond1 cond2  cond3
+ 
+   True  True   True
+   True  False  False
+   False True   False
+   False False  False
+*/
+
+let age = 18;
+age = 30;
+console.log(age >= 18 && age < 30);
+
+// logical OR ||
+/* cond1 cond2  cond3
+ 
+   True  True   True
+   True  False  True
+   False True   True
+   False False  False
+*/
+
+let age1 = 18;
+age1 = 30;
+console.log(age1 >= 18 || age1 <= 30);
+
+// logical NOT !
+// Gives the Opposite result if the condition is true it returns false it the condition is false it returns true
+console.log(!(10 > 4));
