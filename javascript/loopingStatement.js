202f47856e8decf51581a46a5a2aeb0038e88410
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-10-29 15:09:08 +0530

    for loop

---
+++ javascript/loopingStatement.js
@@ -0,0 +1,12 @@
+// for loop 
+// for (intialization; condition ; updation){
+
+// }
+
+// for(let i = 2; i<=10;i++){
+//     console.log ( i );
+// }
+
+for(let i = 2; i<=10;i++){
+    if(i%2 === 0) console.log(i);
+}
