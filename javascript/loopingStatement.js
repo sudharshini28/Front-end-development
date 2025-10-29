df8d7931ce47423d92415173d0e2867323332390
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-10-29 15:26:42 +0530

    While Loop

--- javascript/loopingStatement.js
+++ javascript/loopingStatement.js
@@ -10,3 +10,26 @@
 for(let i = 2; i<=10;i++){
     if(i%2 === 0) console.log(i);
 }
+
+//While Loop
+
+// intialization;
+// while(condition){
+//     Statement;
+//     updation;
+// }
+let val = 10;
+while(val>=1){
+    console.log(val);
+    val--;
+}
+
+let num = 1234;
+let sum = 0;
+
+while(num > 0){
+    let last = num %10; // 1234 % 10 ==> 1230
+    num = parseInt( num/10); // 123 * 10 ==> 1230 parsseInt removes the decimal value
+    sum += last;
+}
+console.log(sum);
