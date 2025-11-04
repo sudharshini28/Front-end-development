702dfb35a351e448431a904ec2c484b3112f1efe
sudharshini28 <sudharshiniarumugam12@gmail.com>
2025-11-04 23:21:31 +0530

    Default parameters

---
+++ javascript/function.js
@@ -0,0 +1,19 @@
+// //Global Variable
+// let person1 ={
+//     name1: "Sudha",
+//     age:20
+// }
+// //Local Variable 
+
+// function printUserName(Uname, Uage){
+//     // console.log("Hello Everyone", person1.name1,person1.age);
+//     console.log(`Hi ${uName}, you age is ${UAge}`);
+// }
+// printUserName("Sudharshini",20);
+// printUserName("Balaji",23);
+
+//Default parameters
+
+let employeeID;
+let newID = employeeID || "UN123";
+console.log(newID);
